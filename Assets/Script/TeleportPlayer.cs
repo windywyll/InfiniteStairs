7b211b8f5e945940c4613234efc6a8dfd7e89f49
Windywyll <mauricejrd@gmail.com>
2016-12-07 19:23:51 +0100

    scripted the tp

---
+++ Assets/Script/TeleportPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportPlayer : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject teleportPoint;
+
+    private float yMove;
+
+    void OnTriggerEnter(Collider _col)
+    {
+        if(_col.transform.root.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            _col.transform.root.Translate(new Vector3(0, yMove, 0));
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        yMove = teleportPoint.transform.position.y - transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
